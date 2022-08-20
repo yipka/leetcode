60e1ae98c0d72fe1179bdb5f4b047c2f9acf1df1
yipka <yejiayiregwoo@gmail.com>
2022-08-20 05:49:54 -0700

    brute force solution

--- src/leetcode/editor/en/[1]Two Sum.java
+++ src/leetcode/editor/en/[1]Two Sum.java
@@ -49,7 +49,16 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        /*brute force solution: traverse*/
+        for(int i = 0;i < nums.length - 1;i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if((nums[i] + nums[j] == target)){
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
